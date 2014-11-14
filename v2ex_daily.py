a48ea71cb0775b59f436d03da2e9e499476b984c
iSayme <isaymeorg@gmail.com>
2014-11-14 12:10:28 +0800

    aha, it works now~

--- v2ex_daily.py
+++ v2ex_daily.py
@@ -3,12 +3,15 @@
 
 import sys
 import re
+import cookielib
 import urllib2
 
-# copy your v2ex cookies here after login
+# your v2ex cookie value for key [auth] after login
+# refer README.md if cannot find cookie [auth]
 V2EX_COOKIE = ''
 
-V2EX_URL_START = r'https://v2ex.com'
+V2EX_DOMAIN = r'v2ex.com'
+V2EX_URL_START = r'https://' + V2EX_DOMAIN
 V2EX_MISSION = V2EX_URL_START + r'/mission/daily'
 V2EX_COIN_URL = r'/mission/daily/redeem?once='
 
@@ -20,10 +23,37 @@ def get_once_url(data):
     else:
         return None
         
+def make_cookie(name, value):
+    return cookielib.Cookie(
+        version=0,
+        name=name,
+        value=value,
+        port=None,
+        port_specified=False,
+        domain=V2EX_DOMAIN,
+        domain_specified=True,
+        domain_initial_dot=False,
+        path='/',
+        path_specified=True,
+        secure=False,
+        expires=None,
+        discard=False,
+        comment=None,
+        comment_url=None,
+        rest=None
+    )
+    
 if __name__ == '__main__':
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:20.0) Gecko/20100101 Firefox/20.0'),
-                         ('Cookie', V2EX_COOKIE)]
+    cj = cookielib.CookieJar()
+    cj.set_cookie(make_cookie('auth', V2EX_COOKIE))
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+    opener.addheaders = [
+        ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:20.0) Gecko/20100101 Firefox/20.0'),
+        ('Referer', V2EX_MISSION)
+    ]
+    
+    opener.open(V2EX_URL_START).read()
     
     data = opener.open(V2EX_MISSION).read()
     once = get_once_url(data)
@@ -33,5 +63,4 @@ if __name__ == '__main__':
     
     v2ex_coin_url = V2EX_URL_START + once
     print v2ex_coin_url
-
     opener.open(v2ex_coin_url).read()
